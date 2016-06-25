e6b7c17b2c20c1855f2e42eb8ca7873881c6cf49
Mavis Ou <mmmavisss@gmail.com>
2016-06-24 17:12:29 -0700

    Switched to Pug :D

---
+++ public/js/word-cloud-footer.js
@@ -0,0 +1,16 @@
+$.ajax("data/word-cloud-categories.json")
+  .done(function(cloudNavData) {
+    $("#word-cloud-nav h3").on("click", function() {
+      var options = {};
+      $("#word-cloud-nav h3").removeClass("active");
+      $(this).addClass("active");
+
+      var index = $("#word-cloud-nav h3").index($(this));
+      if ( index == 0 ) {
+        makeCloud(options,5);
+      } else {
+        options.wordsToInclude = cloudNavData.categories[index].keyphrases;
+        makeCloud(options,0);
+      }
+    });
+});
